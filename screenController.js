a574cb64297a2675c7f99eb4e418d6e909e2ae50
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-03-31 21:23:23 +0200

    Create details section for a task

--- screenController.js
+++ screenController.js
@@ -3,20 +3,30 @@ export default function screenController(project) {
     const tasksContainer = document.querySelector(".tasks-container");
     tasksContainer.innerHTML = "";
 
-    project.getTasks().forEach((task) => appendTask(task, tasksContainer));
+    project
+      .getTasks()
+      .forEach((task) => tasksContainer.append(createTaskDiv(task)));
   };
 
-  updateTasks();
+  const appendTask = (task) => {
+    const tasksContainer = document.querySelector(".tasks-container");
+
+    tasksContainer.append(createTaskDiv(task));
+  };
 
   return {
     updateTasks,
+    appendTask,
   };
 }
 
-function appendTask(task, destination) {
+function createTaskDiv(task) {
+  // TOP SECTION
   let taskDiv = document.createElement("div");
   taskDiv.classList.add("task");
-  taskDiv.append(task.getId());
+
+  let topSection = document.createElement("div");
+  topSection.classList.add("task-top-section");
 
   let checkboxDiv = document.createElement("div");
   checkboxDiv.classList.add("task-checkbox-box");
@@ -38,7 +48,49 @@ function appendTask(task, destination) {
 
   titleDiv.append(title);
 
-  taskDiv.append(checkboxDiv);
-  taskDiv.append(titleDiv);
-  destination.append(taskDiv);
+  topSection.append(task.getId());
+  topSection.append(checkboxDiv);
+  topSection.append(titleDiv);
+
+  taskDiv.append(topSection);
+
+  // TOP SECTION END
+
+  // DETAILS SECTION
+
+  let detailsSection = document.createElement("div");
+  detailsSection.classList.add("task-details-section");
+
+  if (task.getDescription() != "") {
+    let descriptionDiv = document.createElement("div");
+    descriptionDiv.classList.add("task-description-box");
+    let descriptionHeading = document.createElement("h1");
+    descriptionHeading.textContent = "Description";
+    let description = document.createElement("p");
+    description.textContent = task.getDescription();
+
+    descriptionDiv.append(descriptionHeading, description);
+    detailsSection.append(descriptionDiv);
+  }
+
+  let priorityDiv = document.createElement("div");
+  priorityDiv.classList.add("task-priority-box");
+  let priorityHeading = document.createElement("h1");
+  priorityHeading.textContent = "Priority";
+  let priority = document.createElement("p");
+  priority.textContent = task.getPriority();
+
+  priorityDiv.append(priorityHeading, priority);
+  detailsSection.append(priorityDiv);
+  detailsSection.classList.add("hidden");
+
+  taskDiv.append(detailsSection);
+  // DETAILS SECTION END
+
+  taskDiv.addEventListener("click", function () {
+    detailsSection.classList.toggle("hidden");
+    taskDiv.classList.toggle("task-expanded");
+  });
+
+  return taskDiv;
 }
