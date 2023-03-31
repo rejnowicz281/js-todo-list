f0033c1384211b804061bd243b1a150ab1f910d8
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-03-31 21:21:49 +0200

    Add more properties to a task

--- task.js
+++ task.js
@@ -1,10 +1,19 @@
-export default function task(title, completed = false) {
+export default function task(title, description, priority, completed = false) {
+  if (title == "" || priority == "") throw new Error("Invalid task paramaters");
+
   const toggleCompletion = () =>
     completed == true ? (completed = false) : (completed = true);
 
+  const setPriority = (value) => {
+    if (value == "low" || value == "mid" || value == "high") priority = "value";
+  };
+
   return {
     getTitle: () => title,
+    getDescription: () => description,
+    getPriority: () => priority,
     getCompletion: () => completed,
+    setPriority,
     toggleCompletion,
   };
 }
