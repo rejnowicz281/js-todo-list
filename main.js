103b2a64fd80995bbb66458b82805f471e971a6b
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-03-31 21:23:55 +0200

    Update add-task-button functionality for new task paramaters

--- main.js
+++ main.js
@@ -9,7 +9,14 @@ document
   .getElementById("add-task-button")
   .addEventListener("click", function () {
     const title = document.getElementById("title-input").value;
+    const description = document.getElementById("description-input").value;
+    const priority = document.getElementById("priority-input").value;
 
-    currentProject.addTask(task(title));
-    screenController(currentProject).updateTasks();
+    let newTask = task(title, description, priority);
+
+    currentProject.addTask(newTask);
+    let projectTask =
+      currentProject.getTasks()[currentProject.getTasks().length - 1]; // get the task that was added to current project last
+
+    screenController(currentProject).appendTask(projectTask);
   });
